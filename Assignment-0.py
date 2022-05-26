51ad02a4fbdb520409d4d7b3b5359282739a4328
juliasyyyyy <104454765+juliasyyyyy@users.noreply.github.com>
2022-05-27 01:32:49 +0800

    Add files via upload

---
+++ Assignment-0.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+x=int(input("1st number"))
+y=int(input("2nd number"))
+z=int(input("3rd number"))
+sum=x+y+z 
+average=float(sum/3)
+print(average)
+
+
+# # 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
